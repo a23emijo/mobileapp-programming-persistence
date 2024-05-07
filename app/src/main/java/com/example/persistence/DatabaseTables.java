a2068398536685e3c5bec824bdb2dca909d9266b
a23emijo <a23emijo@student.his.se>
2024-05-07 11:56:17 +0200

    The read button can now read data from the database and display it in the textview

--- app/src/main/java/com/example/persistence/DatabaseTables.java
+++ app/src/main/java/com/example/persistence/DatabaseTables.java
@@ -7,15 +7,16 @@ class DatabaseTables {
         static final String COLUMN_NAME_ID = "id";
         static final String COLUMN_NAME_NAME = "name";
         static final String COLUMN_NAME_COLOR = "color";
-        static final String COLUMN_NAMN_NUMBER_OF_LEGS = "numberOfLegs";
+        static final String COLUMN_NAME_NUMBER_OF_LEGS = "numberOfLegs";
     }
 
     static final String SQL_CREATE_TABLE_ANIMAL =
-            // "CREATE TABLE animal (id INTEGER PRIMARY KEY, name TEXT, color TEXT)"
+            // "CREATE TABLE animal (id INTEGER PRIMARY KEY, name TEXT, color TEXT, numberOfLegs TEXT)"
             "CREATE TABLE " + Animal.TABLE_NAME + " (" +
                     Animal.COLUMN_NAME_ID + " INTEGER PRIMARY KEY," +
                     Animal.COLUMN_NAME_NAME + " TEXT," +
-                    Animal.COLUMN_NAME_COLOR + " TEXT)";
+                    Animal.COLUMN_NAME_COLOR + " TEXT," +
+                    Animal.COLUMN_NAME_NUMBER_OF_LEGS + " INT)";
 
     static final String SQL_DELETE_TABLE_ANIMAL =
             // "DROP TABLE IF EXISTS animal"
--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.persistence;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -10,6 +11,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private SQLiteDatabase database;
@@ -19,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText addName;
     private EditText addColor;
     private EditText addNumberOfLegs;
+    private TextView readView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,17 +41,40 @@ public class MainActivity extends AppCompatActivity {
         addColor = findViewById(R.id.middleEditText);
         addNumberOfLegs = findViewById(R.id.rightEditText);
 
+        readView = findViewById(R.id.readView);
+
         writeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ContentValues values = new ContentValues();
                 values.put(DatabaseTables.Animal.COLUMN_NAME_NAME, addName.getText().toString());
                 values.put(DatabaseTables.Animal.COLUMN_NAME_COLOR, addColor.getText().toString());
-                values.put(DatabaseTables.Animal.COLUMN_NAMN_NUMBER_OF_LEGS, addNumberOfLegs.getText().toString());
+                values.put(DatabaseTables.Animal.COLUMN_NAME_NUMBER_OF_LEGS, addNumberOfLegs.getText().toString());
                 database.insert(DatabaseTables.Animal.TABLE_NAME, null, values);
                 addName.setText("");
                 addColor.setText("");
                 addNumberOfLegs.setText("");
+                Log.d("erik", "hej hej");
+            }
+        });
+
+        readButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor cursor = database.query(DatabaseTables.Animal.TABLE_NAME, null, null, null, null, null, null);
+                ArrayList<String> animals = new ArrayList<>();
+                while (cursor.moveToNext()) {
+                    animals.add("ID: " + cursor.getString(cursor.getColumnIndexOrThrow(DatabaseTables.Animal.COLUMN_NAME_ID)) +
+                            " Name: " + cursor.getString(cursor.getColumnIndexOrThrow(DatabaseTables.Animal.COLUMN_NAME_NAME)) +
+                            " Color: " +  cursor.getString(cursor.getColumnIndexOrThrow(DatabaseTables.Animal.COLUMN_NAME_COLOR)) +
+                            " Number of legs: " + cursor.getString(cursor.getColumnIndexOrThrow(DatabaseTables.Animal.COLUMN_NAME_NUMBER_OF_LEGS)) + "\n");
+                }
+                String textAnimals = "";
+                for (String string : animals){
+                    textAnimals  = textAnimals + string;
+                }
+                readView.setText(textAnimals);
+                cursor.close();
             }
         });
     }
